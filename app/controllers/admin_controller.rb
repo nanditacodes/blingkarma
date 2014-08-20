ee5af0263ed86201573139a8727eaf53c0c92ed8
Nandita Rajagopalan <nandita@Nanditas-MacBook-Pro.local>
2014-08-19 23:16:12 -0500

    high charts stuff

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -12,5 +12,16 @@ class AdminController < ApplicationController
 
   def order_index
     @orders = Order.all.group_by(&:order_status)
+    @my_array= []
+
+    ["shipped", "paid", "pending", "cancelled"].each do |key|
+      if @orders[key].present?
+        @my_array << @orders[key].count
+      else
+        @my_array << 0
+      end
+    end
+
   end
+
 end
